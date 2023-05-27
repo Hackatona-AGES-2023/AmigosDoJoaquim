28b72c28ed90a1c49c4b2971e7540b3b77f29fab
Your Name <you@example.com>
2023-05-27 16:08:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
+import express from "express";
+
+
+const app = express()
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const configuration = new Configuration({
+    apiKey: "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"
+});
+
+const openai = new OpenAIApi(configuration);
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+var history = [];
+
+history.push({
+    role: 'system',
+    content: 'Você é um professor tentando ensinar uma criança à ler e escrever. Faça apenas uma pergunta por resposta e só passe para o próximo nível de complexidade quando o aluno apresentar ter absorvido o conteúdo, mesmo que ele precise realizar várias tarefas, mas sempre ajude com dicas. Comece pelo alfabeto, depois passe para as vogais, depois consoantes, depois silabas e então palavras completas. Lembre-se que está falando com uma criança, então seja divertido, animado e simpático. Não use músicas ou canções. Você não pode utilizar palavras ou temas que sejam inadequados para crianças.'
+})
+
+var response
+
+userInterface.prompt();
+
+app.post('/chat', async (req, res) => {
+    const input = req.body.input;
+    history.push({ role: "user", content: input });
+    const { data } = await openai
+        .createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: history,
+        })
+    history.push(data.choices[0].message);
+    response = data.choices[0].message.content;
+    res.send({
+        response
+    })
+})
+
+app.listen(4000, () => {
+    console.log('Example app listening on port 4000!')
+})
---
+++ tts.js
@@ -0,0 +1,43 @@
+
+// const textToSpeech = require('@google-cloud/text-to-speech');
+import textToSpeech from '@google-cloud/text-to-speech'
+// dot env
+
+// require('dotenv').config()
+import fs from 'fs';
+import util from 'util'
+// const fs = require('fs');
+// const util = require('util');
+
+const client = new textToSpeech.TextToSpeechClient();
+
+
+async function quickStart(phrase) {
+
+  // Construct the request
+  const request = {
+    "audioConfig": {
+      "audioEncoding": "LINEAR16",
+      "effectsProfileId": [
+        "small-bluetooth-speaker-class-device"
+      ],
+      "pitch": 1.2,
+      "speakingRate": 1.15
+    },
+    "input": {
+      "text": phrase
+    },
+    "voice": {
+      "languageCode": "pt-BR",
+      "name": "pt-BR-Neural2-A"
+    }
+  };
+
+  // Performs the text-to-speech request
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('output.mp3', response.audioContent, 'binary');
+  console.log('Audio content written to file: output.mp3');
+}
+quickStart("Léo é o cara da pucrs");
